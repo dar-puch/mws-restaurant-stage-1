4e57c350b6cf5c69492ebbd05f59d28bc8dec2b6
Daria Puch <dar-puch@users.noreply.github.com>
2018-07-26 15:18:10 +0200

    fix: fetch event listener

--- sw.js
+++ sw.js
@@ -17,12 +17,21 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('restaurant-v1').then(function(cache) {
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    })
-  );
-});
+	event.respondWith(
+	  caches.match(event.request)
+		.then(function(response) {
+		  if (response) {
+			return response;
+		  }
+		  return fetch(event.request);
+
+  }));
+  caches.open('restaurant-v1').then(function(cache) {
+    return fetch(event.request).then(function(response) {
+      cache.put(event.request, response.clone());
+      return response;
+    });
+  });
+
+
+  });
